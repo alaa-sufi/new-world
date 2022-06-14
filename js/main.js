25a8c429835a9a7e99c74491cd20cc81d72d36fb
unknown <alaasufi227@gmail.com>
2022-06-14 16:00:51 +0300

    up

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    document.querySelector(".loading").style.display = "none";
+    document.querySelector("video").play();
+    document.querySelector("audio").play();
+}
